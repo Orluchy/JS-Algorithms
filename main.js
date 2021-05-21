90354f5449b3721d8fd67dfee53984849306e5e8
Favour Oluchi Boi <favourboi86@gmail.com>
2021-05-21 21:30:59 +0100

    script file updated

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 //Number 1
+/*Function to convert Fahr valued numbers to Celsius
+valued numbers. It only takes a number or a string that can be seen as a number
+*/
+let convertFahrToCelsius = (nums) => {
+    if (typeof nums == 'number') {
+        let celsius = ((nums-32)*(5/9));
+        result = Number(celsius.toFixed(4));
+        return (result);
+    }
+    else if (typeof nums == 'object') {
+        if(Array.isArray(nums)) {
+            return `${JSON.stringify(nums)} is not a valid number but a/an array.`
+        }
+        else if (nums === true || nums === false){
+            return `${nums} is not a valid number but a/an boolean.`
+        }
+        else {
+            output = `${JSON.stringify(nums)} is not a valid number but a/an ${typeof nums}.`
+            return (output);
+        }
+    }
+
+    else if(typeof nums == 'string'){
+      let numsDigit = Number(nums);
+        if(isNaN(numsDigit)) {
+            return (`${nums} is not a valid number but a ${typeof nums}.`)
+        }
+        else {
+            let celsius = ((numsDigit-32)*(5/9));
+            result = Number(celsius.toFixed(4));
+            return (result);
+        }
+    }
+        
+};
+ 
 
 
 
-
-
-
-
-
-
-
-
-
+/*/////////////////////////////////////////////////////////
+                            TASK2
+////////////////////////////////////////////////////////*/
 //Number 2
 const myArrofNums = (n) => {
     let mynums = [];
